9b2f0b4694bb656c9016f8408cd2c54586a7e2bc
Pedro <magalhaes.bcn@gmail.com>
2021-06-05 22:55:58 +0100

    sql for exercise table and trigger

--- DDL.sql
+++ DDL.sql
@@ -23,7 +23,6 @@ CREATE TABLE IF NOT EXISTS logs (
 	);
 
 -- TODO: this table might not be needed
-
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS users (
@@ -31,9 +30,28 @@ CREATE TABLE IF NOT EXISTS users (
 	user_name VARCHAR(255) NOT NULL ,
 	user_image LONGBLOB ,
 	user_sex CHAR(1) , -- M for Male, F for Female, O for Other
-	user_dateofbirth DATE ,	
+	user_birth_date DATE ,	
 	PRIMARY KEY (user_id)
 );
+
+
+DROP TABLE IF EXISTS exercises;
+
+CREATE TABLE IF NOT EXISTS exercises (
+	exer_id BINARY(16) NOT NULL ,
+	exer_timestamp_creation DATETIME ,
+	exer_timestamp_update DATETIME ,
+	exer_name VARCHAR(255) ,
+	exer_url TEXT ,
+	exer_image LONGBLOB ,
+	exer_description TEXT ,
+	exer_video_url TEXT ,
+	bodypart_id BINARY(16) ,
+	equipment_id BINARY(16) ,
+	user_id VARCHAR(255) ,
+	PRIMARY KEY (exer_id)
+);
+
 
 /*** TRIGGERS ***/
 
@@ -41,6 +59,11 @@ CREATE TRIGGER before_insert_logs
   BEFORE INSERT ON logs
   FOR EACH ROW
   SET new.log_id = uuid();
+ 
+ CREATE TRIGGER before_insert_exercise
+  BEFORE INSERT ON exercises
+  FOR EACH ROW
+  SET new.exer_id = uuid();
 
  
 /**** FOREIGN KEYS ***/
